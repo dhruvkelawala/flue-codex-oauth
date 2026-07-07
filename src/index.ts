11c29d7d6ceedc2f1e33e7d8749f06c67d5a2f14
Dhruv Kelawala <dhruvrk2000@gmail.com>
2026-07-07 19:08:49 +0100

    feat(core): export credential store API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export {};
+export {
+  expandHome,
+  readAuthStatus,
+  resolveApiKey,
+  validateAuthPath,
+  writeAuthFileAtomic,
+} from "./credential-store.js";
+export type {
+  AuthCheck,
+  CodexAuthStatus,
+  CodexCredentialStoreOptions,
+} from "./credential-store.js";
